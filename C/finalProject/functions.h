92eea9ee7cde80c68b5ce3c00e70a2911ac91bbc
Fernando G <fernando.gomez@utp.edu.co>
2024-12-01 17:31:04 -0500

    crearPerfilEstudiantes, accionesEstudiantes, estructuras movidas a functions.h

--- C/finalProject/functions.h
+++ C/finalProject/functions.h
@@ -1,6 +1,41 @@
 #ifndef MENUS_H
 #define MENUS_H
 
+// Estructura para Estudiante
+typedef struct
+{
+  int cedula;
+  char nombre[50];
+  int edad;
+  char correo[50];
+} Estudiante;
+
+// Estructura para Docente
+typedef struct
+{
+  int cedula;
+  char nombre[50];
+  char especialidad[50];
+  char correo[50];
+} Docente;
+
+// Estructura para Materia
+typedef struct
+{
+  char codigo[15];
+  char nombre[50];
+  int creditos;
+  char docenteCedula[15];
+} Asignatura;
+
+// Estructura para Nota
+typedef struct
+{
+  int estudianteCedula;
+  char materiaCodigo[10];
+  float nota;
+} Nota;
+
 // Declaraciones de las funciones de los menús
 // Declaración de funciones
 void menus();
